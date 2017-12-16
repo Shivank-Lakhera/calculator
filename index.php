8fedea3b192ac8747b6f1cb00d66ece770c99e1c
Shivank-Lakhera <shivanklakhera@gmail.com>
2017-12-16 20:12:04 +0530

    + operator has been added

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+if(isset($_POST['sub']))
+{
+	$txt1=$_POST['n1'];
+	$txt2=$_POST['n2'];
+	$oprnd=$_POST['sub'];
+	
+	if($oprnd=="+")
+    {
+        $res=$txt1+$txt2;
+        echo $res;
+    }
+	
+}
+?>
+<html>
+    <form method="post" action="">
+        <h2>Calculator</h2>
+        <br>
+        <input name="n1" value="<?php echo $txt1; ?>">
+        <br>
+        <input name="n2" value="<?php echo $txt2; ?>">
+        <br>
+        <br>
+        <input type="submit" name="sub" value="+">
+
+    </form>
+</html>
